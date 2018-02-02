127af8022a9812d71359a83a3d04542b9c4bfd7b
moly <xaloc@hotmail.com>
2018-02-02 03:16:48 -0800

    Canvis usuari Ubuntu

--- src/Carpeta/Principal.java
+++ src/Carpeta/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Modificació des de Windows
+		
+		// L'usuari de Ubuntu també el modifica.
+		
 		System.out.println("Primer Projecte Git!!!!");
 	}
 
